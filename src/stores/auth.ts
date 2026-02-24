746c0164203ab77a439d9eb70abee640fc6e6bd6
Georgebruh <gpsenagan@up.edu.ph>
2026-02-25 06:37:33 +0800

    Files set-up

---
+++ src/stores/auth.ts
@@ -0,0 +1,19 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('auth', () => {
+  const activeRole = ref<'freelancer' | 'client'>('freelancer')   // default freelancer for now so you can build that view first
+  
+  // Mock user data
+  const user = ref({
+    name: 'George Kristan Señagan',
+    email: 'george@projectseal.com',
+    avatar: 'https://via.placeholder.com/40'
+  })
+
+  function toggleRole() {
+    activeRole.value = activeRole.value === 'freelancer' ? 'client' : 'freelancer'
+  }
+
+  return { activeRole, user, toggleRole }
+})
